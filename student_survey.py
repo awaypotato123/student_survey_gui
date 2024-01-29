54b63b7aca97f4187733403497d4c12f02e4bb40
Riyadh <riyadhahmed324@gmail.com>
2024-01-29 10:10:50 -0500

    added label1

--- student_survey.py
+++ student_survey.py
@@ -8,11 +8,26 @@ root.title("ICET Student Survey")
 frame = ttk.Frame(
     root,
     width = 360,
-    height = 240
+    height = 240,
+    
 )
 frame.grid()
 
 frame['padding'] = (5,10)
 frame['borderwidth'] = 10
+
+
+#creating label 1
+
+lbl_full_name = ttk.Label(
+    frame,
+    text='Full name:').grid(
+    column=0,
+    row=1,
+    sticky=(W,E))
+    
+username = StringVar()
+
+
 
 root.mainloop()
